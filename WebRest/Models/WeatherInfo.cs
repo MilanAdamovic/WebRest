9086c24d24a81d0ce52adf86eeee5b685680a3bc
MilanAdamovic <37418775+MilanAdamovic@users.noreply.github.com>
2018-12-16 00:39:01 +0100

    Initial commit

---
+++ WebRest/Models/WeatherInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WebRest.Models
+{
+    [DataContract]
+    public class WeatherInfo
+    {
+        
+        public string Location { get; set; }
+        
+        public float Degree { get; set; }
+        
+
+        public DateTime DateTime { get; set; }
+
+    }
+}
